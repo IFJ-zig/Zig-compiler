062b7373a77a704176e4b935981b9698705d79eb
avozda <adamvozda88@seznam.cz>
2024-10-20 22:40:02 +0200

    add check correct library import

--- src/syntax_an.c
+++ src/syntax_an.c
@@ -16,11 +16,52 @@ int get_token() {
 int Syntax_analyzer(List *L) {
 	tokenList = L;
 	LActFirst(tokenList);
-	code();
+	seekHeaders();
+	LActFirst(tokenList);
+	program();
 	return 0;
 };
 
+
 int seekHeaders() {
+	get_token();
+	if (token->kw != constant) {
+		return SYNTACTIC_ANALYSIS_ERROR;
+	}
+	get_token();
+	if (token->kw != inbuild) {
+		return SYNTACTIC_ANALYSIS_ERROR;
+	}
+
+	get_token();
+	if (token->kw != compare_equal) {
+		return SYNTACTIC_ANALYSIS_ERROR;
+	}
+	get_token();
+	if (token->kw != at) {
+		return SYNTACTIC_ANALYSIS_ERROR;
+	}
+	get_token();
+	if (token->kw != _import) {
+		return SYNTACTIC_ANALYSIS_ERROR;
+	}
+	get_token();
+	if (token->kw != lbracket) {
+		return SYNTACTIC_ANALYSIS_ERROR;
+	}
+	get_token();
+	if (token->kw != text && strcmp(token->s, "ifj24.zig")) {
+		return SYNTACTIC_ANALYSIS_ERROR;
+	}
+	get_token();
+	if (token->kw != rbracket) {
+		return SYNTACTIC_ANALYSIS_ERROR;
+	}
+	get_token();
+	if (token->kw != next) {
+		return SYNTACTIC_ANALYSIS_ERROR;
+	}
+	printf("Headers OK\n");
 	return 0;
 };
 
--- src/syntax_an.h
+++ src/syntax_an.h
@@ -6,9 +6,14 @@
 #ifndef SYNTAX_H
 #define SYNTAX_H
 
+#include "errors_enum.h"
 #include "tokens.h"
+
 
 int Syntax_analyzer(List *L);
 int code();
+int program();
+int seekHeaders();
+
 
 #endif // SYNTAX_H
