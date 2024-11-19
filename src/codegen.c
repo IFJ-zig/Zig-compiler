97c0ccaa39703b94568a2fbcaf9b8d94632b8e28
Daniel Bohata <daniel.b03@seznam.cz>
2024-11-19 18:53:21 +0100

    Created two header functions

--- src/codegen.c
+++ src/codegen.c
@@ -7,3 +7,33 @@
 void printHeader() {
     printf(".IFJcode24\n");
     printf("JUMP main\n");
+    printf("\
+        \
+        LABEL int2float\n\
+        CREATEFRAME\n\
+        PUSHFRAME\n\
+        \
+        DEFVAR LF@retValue\n\
+        DEFVAR LF@param\n\
+        POPS LF@param\n\
+        INT2FLOAT LF@retVal LF@param\n\
+        \
+        POPFRAME\n\
+        RETURN\n\
+        \
+        \
+        LABEL float2int\n\
+        CREATEFRAME\n\
+        PUSHFRAME\n\
+        \
+        DEFVAR LF@retVal\n\
+        DEFVAR LF@param\n\
+        POPS LF@param\n\
+        FLOAT2INT LF@retVal LF@param\n\
+        \
+        POPFRAME\n\
+        RETURN\n\
+        \
+        \
+        \
+    ");
