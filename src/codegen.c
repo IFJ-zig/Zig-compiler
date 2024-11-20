ee0b82b09f517c3bbc04b4f4b0187e45dc843eb5
Daniel Bohata <daniel.b03@seznam.cz>
2024-11-20 01:44:38 +0100

    Added more builtin functions and started debugging them, also added examples

--- src/codegen.c
+++ src/codegen.c
@@ -4,10 +4,31 @@
 #include <stdlib.h> 
 #include <string.h>
 
+void stringToSlice(char* ){
+    
+}
+
 void printHeader() {
     printf(".IFJcode24\n");
     printf("JUMP main\n");
     printf("\
+        \
+        LABEL readInt \n\
+            CREATEFRAME\n\
+            PUSHFRAME\n\
+            DEFVAR LF@retVal\n\
+            DEFVAR LF@readValue\n\
+            DEFVAR LF@readValueType\n\
+            READ LF@readValue int\n\
+            TYPE LF@readValueType LF@readValue\n\
+            JUMPIFNEQ correct_input_int LF@readValueType nil@nil\n\
+            MOVE LF@retVal nil@nil\n\
+            POPFRAME\n\
+            RETURN\n\
+            LABEL correct_input_int\n\
+            MOVE LF@retVal LF@readValue\n\
+            POPFRAME\n\
+        RETURN\n\
         \
         LABEL int2float\n\
         CREATEFRAME\n\
